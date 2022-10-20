4d55d4dc73ccad4398ce416e3379904a45d8a812
Tom <tom.wang@stud.fh-campuswien.ac.at>
2022-10-20 14:25:41 +0200

    Task 1 done

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -5,10 +5,39 @@ import java.util.Scanner;
 
 public class App {
 
-    // Implement all methods as public static
+public static void oneMonthCalendar(int count, int day) {
+
+    int row = day - 1;
+
+        System.out.print(" ");
+        for (int x = 0; x < day*3-3; x++)
+            System.out.print(" ");
+
+    int i = 1;
+    while (i <= count) {
+        while (row < 7 && i <= count) {
+            if (i >= 9 || row == 6)
+                System.out.print(i + " ");
+            else
+                System.out.print(i + "  ");
+            row++;
+            i++;
+        }
+        System.out.println();
+        if (i < 10)
+            System.out.print(" ");
+        row = 0;
+    }
+
+}
+
+
+
+
+// Implement all methods as public static
 
     public static void main(String[] args) {
-        // test your method implementations here
+       oneMonthCalendar(30, 5); // test your method implementations here
         // make method calls
         // print their results
         // etc.
